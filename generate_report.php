106dcc843d3b9725561dfe37e9a86162f06920a5
Daiwei Lu <daiweilu123@gmail.com>
2013-04-04 21:41:48 -0500

    update other pages

--- generate_report.php
+++ generate_report.php
@@ -1,31 +1,26 @@
-<?php 
-include("header.php");
-  $connection = mysql_connect("localhost","root", "");
-    mysql_select_db("tfscdb", $connection) or die("Cannot open the database");
-      
-    mysql_close();
+<?php
+include("_parts/functions.php");
+// include("_parts/connection.php");
+
+// HTML parts
+include("_parts/html_head.php");
+include("_parts/header.php");
 ?>
-<!doctype html>
-<html lang="en">
-<head></head>
-<body>
+
 <div class="container">
-  <div class="row">
-    <div class="threecol">
-      <!--  Left Sidebar -->
-      <ul id="leftNav">            
-         <li><a href="CreateEvent.php">Create Event</a></li>      
-         <li><a href="GenerateReport.php">Generate Reports</a></li>
-         <li><a href="#">RSVP System</a></li>       
-         <li><a href="#">Download Data</a></li> 
-        </ul>
-    </div>
-    <!-- Form to post data-->
-    <div class="sixcol centerPlate">
-        
-    </div>    
-  </div>
+	<div class="row">
+		<div class="span3">
+			<?php include("_parts/sidebar.php"); ?>
+		</div>
+		
+
+		<!-- Form to post data-->
+		<div class="span6 centerPlate">
+			<!-- index body -->
+			<h1>Welcome to TFSC System</h1>
+			<!-- index body end -->
+		</div>
+	</div>
 </div>
 
-</body>
-</html>
+<?php include("_parts/html_foot.php");
