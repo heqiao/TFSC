83c9345a01ae13bd2ec86db3d843e0d3b7363b6e
Daiwei Lu <daiweilu123@gmail.com>
2013-04-04 20:39:23 -0500

    update create_event.php

--- create_event.php
+++ create_event.php
@@ -1,18 +1,26 @@
-<?php  include("connection.php"); ?>
-<?php include("header.php");
+<?php
+include("_parts/functions.php");
+// include("_parts/connection.php");
 
-if($connection && isset($_POST['submitEvent']))
+// HTML parts
+include("_parts/html_head.php");
+include("_parts/header.php");
+?>
+
+<?php
+if(isset($connection) && isset($_POST['submitEvent']))
 {
-	$eventName 		= strip_tags(trim($_POST['eventName']));
-	$datepicker 	= strip_tags(trim($_POST['datepicker']));
-	$eventLoc 		= strip_tags(trim($_POST['eventLoc']));
-	$eventDesc 		= strip_tags(trim($_POST['Description']));
-	$eventType 		= strip_tags(trim($_POST['selectType']));
-	$eventStart 	= strip_tags(trim($_POST['eventStart']));
-	$eventEnd 		= strip_tags(trim($_POST['eventEnd']));
-	$eventContactName 	= strip_tags(trim($_POST['eventContactName']));
-	$eventContactEmail 	= strip_tags(trim($_POST['eventContactEmail']));
-	$eventContactPhone 	= strip_tags(trim($_POST['eventContactPhone']));
+	$eventName         = strip_tags(trim($_POST['eventName']));
+	$datepicker        = strip_tags(trim($_POST['datepicker']));
+	$eventLoc          = strip_tags(trim($_POST['eventLoc']));
+	$eventDesc         = strip_tags(trim($_POST['Description']));
+	$eventType         = strip_tags(trim($_POST['selectType']));
+	$eventStart        = strip_tags(trim($_POST['eventStart']));
+	$eventEnd          = strip_tags(trim($_POST['eventEnd']));
+	$eventContactName  = strip_tags(trim($_POST['eventContactName']));
+	$eventContactEmail = strip_tags(trim($_POST['eventContactEmail']));
+	$eventContactPhone = strip_tags(trim($_POST['eventContactPhone']));
+
 	//Session Info
 	$sessionDesc = strip_tags(trim($_POST['sessionDesc']));
 	/*Validation for user input*/
@@ -71,170 +79,190 @@ if($connection && isset($_POST['submitEvent']))
 	 }
 }
 ?>
-        <h1 id="main-title">Create an Event</h1><br>
-		
-		<?php if ($okmessage) { ?>
-		<div class="alert alert-success">
-			<button type="button" class="close" data-dismiss="alert">&times;</button>
-			<strong>Successful!</strong>
-			<?php
-			if (isset($typeErro) == ture) {
-				echo $typeErro;
-			}
-			
-			?>
+
+<div class="container">
+	<div class="row">
+		<div class="span3">
+			<?php include("_parts/sidebar.php"); ?>
 		</div>
-		<?php } ?>
 		
-		<form id="main-form" class="form-horizontal" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-			<!-- event name -->
-			<div class="control-group">
-				<label class="control-label" for="eventName">Event Name:</label>
-				<div class="controls">
-					<input type="text" name = "eventName" id="eventName"  placeholder="" required>
-				</div>
-			</div>
-			<!-- date -->
-			<div class="control-group">
-				<label class="control-label" for="datepicker">Date:</label>
-				<div class="controls">
-					<input type="text" id="datepicker" placeholder="">
-				</div>
-			</div>
-			<!-- location -->
-			<div class="control-group">
-				<label class="control-label" for="eventLoc">Location:</label>
-				<div class="controls">
-					<input type="text" id="eventLoc" placeholder="">
-				</div>
-			</div>
-			<!-- desc -->
-			<div class="control-group">
-				<label class="control-label" for="Description">Description:</label>
-				<div class="controls">
-					<textarea name="Description" rows="3"></textarea>
-				</div>
-			</div>
-			<!-- type -->
-			<div class="control-group">
-				<label class="control-label" for="selectType">Event Type:</label>
-				<div class="controls">
-					<select name="selectType" id="selectType"> 
-						<option value="select">--Select One--</option>
-						<option value="TA">Teaching Assistant Luncheon</option>
-						<option value="FACULTY">Luncheon</option>
-						<option value="SYMPOSIUM">Symposium</option>
-						<option value="RETREAT">Teaching Retreat</option>
-					</select>
-				</div>
-			</div>
-		
-			<style type="text/css" media="screen">
-				.break-section {
-					display: none;
-				}
-				.event-section {
-					display: none;
-				}
 
-			</style>
-			<div class = "break-section">
+		<!-- Form to post data-->
+		<div class="span6 centerPlate">
+			<!-- index body -->
+			
+			
+	        <h1 id="main-title">Create an Event</h1><br>
+		
+			<?php if ($okmessage) { ?>
+			<div class="alert alert-success">
+				<button type="button" class="close" data-dismiss="alert">&times;</button>
+				<strong>Successful!</strong>
+				<?php
+				if (isset($typeErro) == ture) {
+					echo $typeErro;
+				}
+			
+				?>
+			</div>
+			<?php } ?>
+		
+			<form id="main-form" class="form-horizontal" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+				<!-- event name -->
 				<div class="control-group">
-					<label class="control-label" for="break-num">Breakout Session Number</label>
+					<label class="control-label" for="eventName">Event Name:</label>
 					<div class="controls">
-						<select name = "break-num" id = "break-num" >
-							<option value="0">--Select One--</option>
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
+						<input type="text" name = "eventName" id="eventName"  placeholder="" required>
+					</div>
+				</div>
+				<!-- date -->
+				<div class="control-group">
+					<label class="control-label" for="datepicker">Date:</label>
+					<div class="controls">
+						<input type="text" id="datepicker" placeholder="">
+					</div>
+				</div>
+				<!-- location -->
+				<div class="control-group">
+					<label class="control-label" for="eventLoc">Location:</label>
+					<div class="controls">
+						<input type="text" id="eventLoc" placeholder="">
+					</div>
+				</div>
+				<!-- desc -->
+				<div class="control-group">
+					<label class="control-label" for="Description">Description:</label>
+					<div class="controls">
+						<textarea name="Description" rows="3"></textarea>
+					</div>
+				</div>
+				<!-- type -->
+				<div class="control-group">
+					<label class="control-label" for="selectType">Event Type:</label>
+					<div class="controls">
+						<select name="selectType" id="selectType"> 
+							<option value="select">--Select One--</option>
+							<option value="TA">Teaching Assistant Luncheon</option>
+							<option value="FACULTY">Luncheon</option>
+							<option value="SYMPOSIUM">Symposium</option>
+							<option value="RETREAT">Teaching Retreat</option>
 						</select>
 					</div>
 				</div>
-			</div>
+		
+				<style type="text/css" media="screen">
+					.break-section {
+						display: none;
+					}
+					.event-section {
+						display: none;
+					}
 
-			<div class="event-section">
-				<div class="control-group">
-					<label class="control-label" for="sessionDesc">Description:</label>
-					<div class="controls">
-						<input type="text" id="sessionDesc" placeholder="">
+				</style>
+				<div class = "break-section">
+					<div class="control-group">
+						<label class="control-label" for="break-num">Breakout Session Number</label>
+						<div class="controls">
+							<select name = "break-num" id = "break-num" >
+								<option value="0">--Select One--</option>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+							</select>
+						</div>
 					</div>
 				</div>
-				<div class="control-group">
-					<label class="control-label" for="sessionSpeaker">Speaker:</label>
-					<div class="controls">
-						<input type="text" id="sessionSpeaker" placeholder="">
+
+				<div class="event-section">
+					<div class="control-group">
+						<label class="control-label" for="sessionDesc">Description:</label>
+						<div class="controls">
+							<input type="text" id="sessionDesc" placeholder="">
+						</div>
 					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="sessionType">Session Type:</label>
-					<div class="controls">
-						<select name = "sessionType" id = "sessionType" >
-							<option value="1">Individual</option>
-						</select>
+					<div class="control-group">
+						<label class="control-label" for="sessionSpeaker">Speaker:</label>
+						<div class="controls">
+							<input type="text" id="sessionSpeaker" placeholder="">
+						</div>
 					</div>
+					<div class="control-group">
+						<label class="control-label" for="sessionType">Session Type:</label>
+						<div class="controls">
+							<select name = "sessionType" id = "sessionType" >
+								<option value="1">Individual</option>
+							</select>
+						</div>
 					
-					<div class="controls">
-					<button class="btn" id="add-session" type="button">Add Session</button>
+						<div class="controls">
+						<button class="btn" id="add-session" type="button">Add Session</button>
+						</div>
 					</div>
 				</div>
-			</div>
-			<style type="text/css" media="screen">
-				.event-session {
-					margin-top: 10px;
-					margin-bottom: 10px;
-					border:1px solid silver;
-				}
-			</style>
+				<style type="text/css" media="screen">
+					.event-session {
+						margin-top: 10px;
+						margin-bottom: 10px;
+						border:1px solid silver;
+					}
+				</style>
 		
-			<script type="text/template" id="session-template" charset="utf-8">
-				<div class="event-session">
-					<span class="session-desc"><%= desc %></span>
-					<span class="session-speaker"><%= speaker %></span>
-					<span class ="session-type"><%= type %></span>
-					<a href="#" class="add-subsession">+</a>
-				</div>
-			</script>
+				<script type="text/template" id="session-template" charset="utf-8">
+					<div class="event-session">
+						<span class="session-desc"><%= desc %></span>
+						<span class="session-speaker"><%= speaker %></span>
+						<span class ="session-type"><%= type %></span>
+						<a href="#" class="add-subsession">+</a>
+					</div>
+				</script>
 			
-			<div class="control-group">
-				<label class="control-label" for="eventStart">Start Time:</label>
-				<div class="controls">
-					<input type="text" id="eventStart" placeholder="">
+				<div class="control-group">
+					<label class="control-label" for="eventStart">Start Time:</label>
+					<div class="controls">
+						<input type="text" id="eventStart" placeholder="">
+					</div>
 				</div>
-			</div>
 			
-			<div class="control-group">
-				<label class="control-label" for="eventEnd">End Time:</label>
-				<div class="controls">
-					<input type="text" id="eventEnd" placeholder="">
+				<div class="control-group">
+					<label class="control-label" for="eventEnd">End Time:</label>
+					<div class="controls">
+						<input type="text" id="eventEnd" placeholder="">
+					</div>
 				</div>
-			</div>
 			
-			<div class="control-group">
-				<label class="control-label" for="eventContactName">Contact Name:</label>
-				<div class="controls">
-					<input type="text" id="eventContactName" placeholder="">
+				<div class="control-group">
+					<label class="control-label" for="eventContactName">Contact Name:</label>
+					<div class="controls">
+						<input type="text" id="eventContactName" placeholder="">
+					</div>
 				</div>
-			</div>
 			
-			<div class="control-group">
-				<label class="control-label" for="eventContactEmail">Contact Email:</label>
-				<div class="controls">
-					<input type="text" id="eventContactEmail" placeholder="">
+				<div class="control-group">
+					<label class="control-label" for="eventContactEmail">Contact Email:</label>
+					<div class="controls">
+						<input type="text" id="eventContactEmail" placeholder="">
+					</div>
 				</div>
-			</div>
 			
-			<div class="control-group">
-				<label class="control-label" for="eventContactPhone">Contact Phone:</label>
-				<div class="controls">
-					<input type="text" id="eventContactPhone" placeholder="">
+				<div class="control-group">
+					<label class="control-label" for="eventContactPhone">Contact Phone:</label>
+					<div class="controls">
+						<input type="text" id="eventContactPhone" placeholder="">
+					</div>
 				</div>
-			</div>
 			
-			<div class="control-group">
-				<div class="controls">
-					<button class="btn" type="submit" name ='submitEvent'>Add Event</button>
+				<div class="control-group">
+					<div class="controls">
+						<button class="btn" type="submit" name ='submitEvent'>Add Event</button>
+					</div>
 				</div>
-			</div>
-		</form>
-<?php include("footer.php");
+			</form>
+			
+			
+			<!-- index body end -->
+		</div>
+	</div>
+</div>
+
+<?php include("_parts/html_foot.php");
